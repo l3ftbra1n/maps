a2857519ff63b1923845578f5b7b11acf0eedbe1
l3ftbra1n <l3ftbra1n@gmail.com>
2022-03-16 11:41:42 -0400

    first commit

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+/// <reference types="@types/google.maps" />
+
+// import { User } from './User';
+// import { Company } from './Company';
+import { CustomMap } from "./CustomMap";
+
+new CustomMap('map');
+
+
